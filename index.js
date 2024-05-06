6732e685bb3037567bb05e04135f2cf61955f1ff
Troverint <indralouay@gmail.com>
2024-05-06 07:33:00 +0700

    mffj

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("louheyyy"));
+app.get("/", (req, res) => res.send("louheyyyy"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
