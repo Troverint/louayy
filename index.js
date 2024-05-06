cf18f2c5b6e29e06a9c933acee3f44c5b1f2ab10
Troverint <indralouay@gmail.com>
2024-05-06 07:34:19 +0700

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("louheyyyy"));
+app.get("/", (req, res) => res.send("louheyyyyy"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
